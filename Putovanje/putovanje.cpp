b160442fac67fabc4888b53ae2a46b71280d6bf4
allen <allen.harper9917@gmail.com>
2019-10-01 16:31:22 -0600

    Submitt putovanje.cpp

--- Putovanje/putovanje.cpp
+++ Putovanje/putovanje.cpp
@@ -1,7 +1,7 @@
 /*
 Author: Allen Harper
 Date: 9/20/19
-Description: Solution for kattis problem; https://open.kattis.com/problems/putovanje
+Description: Solution for kattis problem: https://open.kattis.com/problems/putovanje
 https://docs.google.com/document/d/1ATmHqwDpr81xYQ5ytg-dmLFyXsXpbafoHxfF5B67xa8/edit
 */
 
@@ -13,39 +13,53 @@ https://docs.google.com/document/d/1ATmHqwDpr81xYQ5ytg-dmLFyXsXpbafoHxfF5B67xa8/
 #include <string.h>
 using namespace std;
 
-int solve(){
 
+int solve(int stomach_size, vector<int> food_found){
+    int b = 0;
+    for (int i = 0; i < food_found.size(); i++){
+        int t = 0;
+        int m = 0;
+        for(int j = i; j < food_found.size(); j++){
+            if(t + food_found[j] <= stomach_size){
+                t += food_found[j];
+                m += 1;
+            }
+        }
+        b = max(b, m);
+    }
+    return b;
 }
 
 void test() {
+    int a1 = 5;
+    vector<int> v1 = {3, 1, 2, 1, 1};
+    int a2 = 5;
+    vector<int> v2 = {1, 5, 4, 3, 2, 1, 1};
+    int a3 = 10;
+    vector<int> v3 = {3, 2, 5, 4, 3};
 
+    assert(solve(a1, v1) == 4);
+    assert(solve(a2, v2) == 3);
+    assert(solve(a3, v3) == 3);
     cout << "Tested. All passed...";
 }
 
 int main(int argc, const char** argv) {
     if (argc > 1 && strncmp(argv[1], "test", 4) == 0)
         test();
-    else
-        solve();
+    else{
+        // Pull data in
+        vector<int> food_found;
+        int num_of_food, stomach_size, temp;
+        cin >> num_of_food >> stomach_size;
+        for (int i = 0; i < num_of_food; i++)
+        {
+            cin >> temp;
+            food_found.push_back(temp);
+        }
+        cout << solve(stomach_size, food_found);
+    }
     
-    // Pull data in
-    vector<int> food_found;
-    int num_of_food, stomach_size, temp;
-    cin >> num_of_food >> stomach_size;
-    for (int i = 0; i < num_of_food; i++)
-    {
-        cin >> temp;
-        food_found.push_back(temp);
-    }
-
-    // print input
-    cout << "food_found: ";
-    for(auto z : food_found){
-        cout << z << ' ';
-    }
-    cout << "\nnum_of_food: " << num_of_food << endl
-         << "stomach_size: " << stomach_size << endl;
-
     return 0;
 }
 
